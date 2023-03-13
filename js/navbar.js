21a5355343ac73e9da075a1c74d1e1c10408a065
doug <ddoug.allves@gmail.com>
2023-03-13 15:01:04 -0300

    :test_tube: test: Página inicial

--- js/navbar.js
+++ js/navbar.js
@@ -13,7 +13,26 @@ function navbar() {
     hideNav.addEventListener('click',()=>{
         nav.style.left = '-100%';
     }) 
+
+    let cart = ()=>{
+        const offCanvas = document.querySelector('.cart--offcanvas');
+        const show = document.querySelector('.navbar__toggle--cart.navbar__toggle--show')
+        const hide = document.querySelector('.cart__toggle.cart__toggle--hide');
+
+        show.addEventListener('click',()=>{
+            offCanvas.style.left = '0';
+        })
+
+        hide.addEventListener('click',()=>{
+            offCanvas.style.left = '-100%';
+        })
+
+    }
+
+    cart();
     
 }
+
+
 
 navbar();
